bc76e79fb7c543b9dd81b22c4273b9afececde76
Ignacio <dev.ignacio.h@gmail.com>
2020-02-25 23:43:58 -0300

    funcionalidad cambiar de foto hacia adelante

--- gallery.js
+++ gallery.js
@@ -1,2 +1,16 @@
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+const galleryImgs = document.querySelectorAll(".gallery-img");
+let currentlySelected = 0;
+
+prevBtn.addEventListener("click", function() {});
+
+nextBtn.addEventListener("click", function() {
+    galleryImgs[currentlySelected].classList.remove('active')
+    currentlySelected++
+    galleryImgs[currentlySelected].classList.add('active')
+    prevBtn.disabled = false;
+    if (galleryImgs.length === currentlySelected +1) {
+        nextBtn.disabled = true;
+    }
+});
